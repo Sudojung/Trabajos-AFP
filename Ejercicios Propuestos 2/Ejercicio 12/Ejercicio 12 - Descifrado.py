71e4412cf35b041f924fcc678d75e87f893d336d
Renzo Cienfuegos <areyoustupidorretarded@proton.me>
2026-06-24 19:37:20 -0400

    Refactor decryption code to use array for digits

--- Ejercicios Propuestos 2/Ejercicio 12/Ejercicio 12 - Descifrado.py
+++ Ejercicios Propuestos 2/Ejercicio 12/Ejercicio 12 - Descifrado.py
@@ -4,20 +4,21 @@ GROUP = 2
 # Solicitamos el número cifrado
 numero = input("Ingrese el número cifrado: ")
 
-# Deshacemos los intercambios realizados en el cifrado
-d1 = int(numero[2])
-d2 = int(numero[3])
-d3 = int(numero[0])
-d4 = int(numero[1])
+# Arreglo para almacenar los dígitos
+digitos = [0] * 4
+
+# Restauramos el orden original
+digitos[0] = int(numero[2])
+digitos[1] = int(numero[3])
+digitos[2] = int(numero[0])
+digitos[3] = int(numero[1])
 
 # Recuperamos los dígitos originales
-d1 = (d1 + 3) % 10
-d2 = (d2 + 3) % 10
-d3 = (d3 + 3) % 10
-d4 = (d4 + 3) % 10
+for i in range(4):
+    digitos[i] = (digitos[i] + 3) % 10
 
 # Formamos el número original
-original = f"{d1}{d2}{d3}{d4}"
+original = f"{digitos[0]}{digitos[1]}{digitos[2]}{digitos[3]}"
 
 # Mostramos el resultado
 print(f"Número original: {original}")
