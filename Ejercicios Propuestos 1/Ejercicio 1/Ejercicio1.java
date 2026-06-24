52e90cba1567304e9fc389778abe33e4d7687840
Renato Dario Salcedo Gaviria <renato.salcedog@unmsm.edu.pe>
2026-06-24 08:24:46 -0500

    Add files via upload

---
+++ Ejercicios Propuestos 1/Ejercicio 1/Ejercicio1.java
@@ -0,0 +1,12 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Tabla del 3:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("3 x " + i + " = " + (3 * i));
+        }
+        System.out.println("\nTabla del 9:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("9 x " + i + " = " + (9 * i));
+        }
+    }
+}
