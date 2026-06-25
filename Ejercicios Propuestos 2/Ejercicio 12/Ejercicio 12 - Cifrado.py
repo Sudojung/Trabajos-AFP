92883d1ec0555f9132945d600f108787efa7643b
Renzo Cienfuegos <areyoustupidorretarded@proton.me>
2026-06-24 22:19:39 -0400

    Refactor encryption logic for 4-digit number

--- Ejercicios Propuestos 2/Ejercicio 12/Ejercicio 12 - Cifrado.py
+++ Ejercicios Propuestos 2/Ejercicio 12/Ejercicio 12 - Cifrado.py
@@ -2,17 +2,30 @@ NAME = "Renzo Leonardo Cienfuegos Cardenas"
 GROUP = 2
 
 # Solicitamos un número de 4 dígitos
-numero = input("Ingrese un número de 4 dígitos: ")
+num = int(input("Ingrese el dato original (entero de 4 dígitos): "))
 
-# Arreglo para almacenar los dígitos cifrados
-digitos = [0] * 4
+# Extraer los dígitos
+d1 = num // 1000
+d2 = (num % 1000) // 100
+d3 = (num % 100) // 10
+d4 = num % 10
 
-# Convertimos y ciframos cada dígito
-for i in range(4):
-    digitos[i] = (int(numero[i]) + 7) % 10
+# Sustituir cada dígito por (dígito + 7) % 10
+d1 = (d1 + 7) % 10
+d2 = (d2 + 7) % 10
+d3 = (d3 + 7) % 10
+d4 = (d4 + 7) % 10
 
-# Intercambiamos posiciones
-cifrado = f"{digitos[2]}{digitos[3]}{digitos[0]}{digitos[1]}"
+# Intercambiar el 1.º con el 3.º
+temp = d1
+d1 = d3
+d3 = temp
 
-# Mostramos el resultado
-print(f"Número cifrado: {cifrado}")
+# Intercambiar el 2.º con el 4.º
+temp = d2
+d2 = d4
+d4 = temp
+
+# Mostrar resultado
+print("El número cifrado seguro es:", end=" ")
+print(d1, d2, d3, d4, sep="")
